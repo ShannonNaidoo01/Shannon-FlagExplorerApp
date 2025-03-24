43b861fc22eabcc1ba94729f388f20e2fae43cff
Shannon Naidoo <shannon.naidoo+GGL@gamesglobal.com>
2025-03-24 15:00:00 +0200

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // Import this to use the matchers
 import App from './App';
 
 test('renders Country Flags title', () => {
